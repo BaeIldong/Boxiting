3b9139c1016aa20352fd05cc1d286b8aae750a77
BaeIldong <idbae@ark.kr>
2023-08-01 15:14:39 +0900

    test

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
 //push test
+
+//함수선언 방법1
+function test(name){
+    
+}
+
+//함수선언 방법2
+const test = function () {
+
+}
+//함수선언 방법3
+const test = function orgName() {
+
+}
+
+//함수선언 방법4
+const test = (function(object=1){return object})
+
+//함수선언 방법5
+const test = () => {}
+
+//함수선언 방법6
+const test = (a,b,c) => {a,b,c}
+
+
+const test = (a, b, c) => a+b+c
